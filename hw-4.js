3aca0ac29cc262753f6d462b64814ddfde85e0b3
MariaBob91 <152320466+MariaBob91@users.noreply.github.com>
2024-01-15 23:06:17 +0500

    Теоритическая часть 2.5 готова

--- hw-4.js
+++ hw-4.js
@@ -41,3 +41,49 @@ let firstFriday = 5;
 for (let i = firstFriday; i <= 31; i += 7) {
     console.log('Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.');
 }
+
+
+
+const user = {
+    name: 'Bob',
+    age: 32,
+    city: 'London',
+
+    getInfo() {
+        return `Пользователь ${user.name}, возраст ${user.age}, проживает в ${user.city}`
+    }
+};
+
+console.log(user.getInfo());
+
+
+
+
+
+function getRectangleArea() {
+    return this.width * this.height;
+}
+function getRectanglePerimeter() {
+    return this.width * 2 + this.height * 2;
+}
+
+const square1 = {
+    width: 23,
+    height: 5,
+
+    getArea: getRectangleArea,
+    getPerimeter: getRectanglePerimeter
+};
+
+const square2 = {
+    width: 5,
+    height: 35,
+
+    getArea: getRectangleArea,
+    getPerimeter: getRectanglePerimeter
+};
+
+console.log(square1.getArea());
+console.log(square2.getPerimeter());
+console.log(square1.getArea());
+console.log(square2.getPerimeter());
---
+++ hw-5.js
@@ -0,0 +1,111 @@
+//Задание 1
+
+function comparison(a, b) {
+    return a < b ? a : b;
+}
+
+console.log(comparison(2, 5));
+
+//Задание 2
+function putNumber(n) {
+
+    if (n % 2 === 0) {
+        return('Число четное');
+    } else {
+        return('Число нечетное');
+    }
+}
+
+console.log(putNumber(2));
+
+//Задание 3.1
+function square(n) {
+    console.log(n * n);
+};
+
+square(2);
+
+//Задание 3.2
+function square2(n) {
+    return n * n;
+}
+
+console.log(square2(5));
+
+//Задание 4
+function userAge() {
+    const age = prompt('Сколько вам лет?');
+
+    if (age < 0) {
+        alert('Вы ввели неправильное значение');
+    } else if (age >= 0 && age <= 12) {
+        alert('Привет, друг!');
+    } else {
+        alert('Добро пожаловать!')
+    }
+}
+
+//Задание 5
+function getNumber(a, b) {
+    if (isNaN(Number(a)) || isNaN(Number(b))) {
+        return('Одно или оба значения не являются числом');
+    } else {
+        return a * b
+    }
+}
+
+//Задание 6
+function cubeNumber() {
+    let num = prompt("Введите число");
+    num = Number(num);
+
+    if (isNaN(num)) {
+        return('Переданный параметр не является числом');   
+    } else {
+        return `${num} в кубе равняется ${num ** 3}`;
+    }
+}
+
+console.log(cubeNumber(2));
+
+//Задание 7
+function getArea() {
+    return Math.PI * this.radius * this.radius;
+}
+
+function getPerimeter() {
+    return 2 * Math.PI * this.radius;
+}
+
+const circle1 = {
+    radius: 3,
+    getArea,
+    getPerimeter
+};
+console.log(circle1.getPerimeter());
+
+const circle2 = {
+    radius: 10,
+    getArea,
+    getPerimeter
+};
+
+console.log(circle2.getPerimeter());
+
+//Задание 8
+function monthNumber(month) {
+
+    if (month >= 3 && month <= 5) {
+        return "Весна";
+    } else if (month >= 6 && month <= 8) {
+        return "Лето";
+    } else if (month >= 9 && month <= 11) {
+        return "Осень";
+    } else if (month == 12 && month == 1 && month == 2) {
+        return "Зима";
+    } else {
+        return "Неверное значение";
+    }
+}
+
+alert(monthNumber(prompt("Введите число от 1 до 12")));
